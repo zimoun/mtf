7c925d03055e343960cfff42c92fc57268d7eefb
zimoun <zimoun.toutoune@gmail.com>
2016-05-19 13:38:41 -0400

    refactoring of Error_by_spaces

--- check_Error_by_spaces.py
+++ check_Error_by_spaces.py
@@ -8,18 +8,27 @@ import scipy.sparse.linalg as spla
 
 import bempp.api as bem
 
-def check_proj(diri, neum):
-    tt = time()
+
+def create_spaces(diri, neum):
 
     trial, test = diri
     triad = bem.function_space(grid, trial[0], trial[1])
     testd = bem.function_space(grid, test[0], test[1])
+    spaced = (triad, testd)
 
     trial, test = neum
     trian = bem.function_space(grid, trial[0], trial[1])
     testn = bem.function_space(grid, test[0], test[1])
+    spacen = (trian, testn)
 
+    return spaced, spacen
 
+def get_opAi(spaces, k, sign=1):
+    tt = time()
+
+    spaced, spacen = spaces
+    triad, testd = spaced
+    trian, testn = spacen
 
     opK = bem.operators.boundary.helmholtz.double_layer(triad, triad, testd, k)
     opV = bem.operators.boundary.helmholtz.single_layer(trian, triad, testd, k)
@@ -27,30 +36,76 @@ def check_proj(diri, neum):
     opQ = bem.operators.boundary.helmholtz.adjoint_double_layer(trian, trian, testn, k)
 
     opA = bem.BlockedOperator(2, 2)
-    opA[0, 0], opA[0, 1] = -opK, opV
-    opA[1, 0], opA[1, 1] = opW, opQ
+    opA[0, 0], opA[0, 1] = - sign * opK, opV
+    opA[1, 0], opA[1, 1] = opW, sign * opQ
+
+    #print("collect A:", time()-tt)
+    return opA
+
+
+def get_Xij(spaces):
+    tt = time()
+
+    spaced, spacen = spaces
+    triad, testd = spaced
+    trian, testn = spacen
 
     opId = bem.operators.boundary.sparse.identity(triad, triad, testd)
     opIn = bem.operators.boundary.sparse.identity(trian, trian, testn)
+
+    opX = bem.BlockedOperator(2, 2)
+    opX[0, 0], opX[1, 1] = opId, -opIn
+
+    #print("collect X:", time()-tt)
+    return opX
+
+
+def get_opJi(spaces):
+    tt = time()
+
+    spaced, spacen = spaces
+    triad, testd = spaced
+    trian, testn = spacen
+
+    opId = bem.operators.boundary.sparse.identity(triad, triad, testd)
+    opIn = bem.operators.boundary.sparse.identity(trian, trian, testn)
+
     opJ = bem.BlockedOperator(2, 2)
     opJ[0, 0], opJ[1, 1] = opId, opIn
 
-    Idw = opId.weak_form()
-    IId = Idw.sparse_operator
-    IId = IId.astype(complex)
-    iIId = spla.splu(IId)
-    iIdl = spla.LinearOperator(IId.shape, matvec=iIId.solve, dtype=complex)
+    #print("collect J:", time()-tt)
+    return opJ
 
-    Inw = opIn.weak_form()
-    IIn = Idw.sparse_operator
-    IIn = IId.astype(complex)
-    iIIn = spla.splu(IIn)
-    iInl = spla.LinearOperator(IIn.shape, matvec=iIIn.solve, dtype=complex)
+def get_iJi(spaces):
+    tt = time()
+
     iJ = bem.BlockedDiscreteOperator(2, 2)
-    iJ[0, 0], iJ[1, 1] = iIdl, iInl
 
+    opJ = get_opJi(spaces)
+    opId, opIn = opJ[0, 0], opJ[1, 1]
+
+    for ii, opI in enumerate([opId, opIn]):
+        Iw = opI.weak_form()
+        II = Iw.sparse_operator
+        II = II.astype(complex)
+        iII = spla.splu(II)
+        iIl = spla.LinearOperator(II.shape, matvec=iII.solve, dtype=complex)
+        iJ[ii, ii] = iIl
+
+    #print("compute iJ:", time()-tt)
+    return iJ
+
+
+def get_and_check_Ci(spaces, k, sign=1):
+    tt = time()
+
+    opA = get_opAi(spaces, k, sign)
     Aw = opA.weak_form()
+
+    opJ = get_opJi(spaces)
     Jw = opJ.weak_form()
+
+    iJ = get_iJi(spaces)
 
     C = lambda x: 0.5*Jw(x) + Aw(x)
     CC = lambda x: C( iJ( C(x)))
@@ -66,13 +121,14 @@ def check_proj(diri, neum):
           "dof:", Aw.shape[0],
     )
 
+
 if __name__ == "__main__":
 
     k = np.pi
     lmbda = 2 * np.pi / k
 
-    #grid = bem.shapes.sphere(h=lmbda/10)
-    grid = bem.shapes.cube(h=lmbda/10)
+    grid = bem.shapes.sphere(h=lmbda/10)
+    #grid = bem.shapes.cube(h=lmbda/10)
 
     # space
     # (diri, neum)
@@ -81,7 +137,7 @@ if __name__ == "__main__":
     spaces = [
         ((("DP", 0), ("DP", 0)), (("DP", 0), ("DP", 0))),
         ((("P", 1), ("P", 1)), (("P", 1), ("P", 1))),
-        ((("DUAL", 0), ("DUAL", 0)), (("B-P", 1), ("B-P", 1))),
+        ((("B-P", 1), ("B-P", 1)), (("DUAL", 0), ("DUAL", 0))),
 
         ((("DP", 1), ("DP", 1)), (("DP", 1), ("DP", 1))),
         ((("DP", 2), ("DP", 2)), (("DP", 2), ("DP", 2))),
@@ -90,7 +146,9 @@ if __name__ == "__main__":
         ((("P", 3), ("P", 3)), (("P", 3), ("P", 3))),
         ((("B-DP", 1), ("B-DP", 1)), (("B-DP", 1), ("B-DP", 1))),
         ((("B-P", 1), ("B-P", 1)), (("B-P", 1), ("B-P", 1))),
+        ((("DUAL", 0), ("DUAL", 0)), (("B-P", 1), ("B-P", 1))),
     ]
 
     for diri, neum in spaces:
-        check_proj(diri, neum)
+        spaces = create_spaces(diri, neum)
+        get_and_check_Ci(spaces, k)
