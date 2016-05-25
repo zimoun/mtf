aa627f5e19f097d1f3a0b44bb0443db31d361b59
zimoun <zimoun.toutoune@gmail.com>
2016-05-24 22:44:07 -0400

    operator preconditioning is not an option!

--- eg/helm_dual_gmres.py
+++ eg/helm_dual_gmres.py
@@ -40,8 +40,8 @@ def solve(Mat, rhs, tol=1e-5, Ml=None, force_left=False, check=False):
     ok = True
     toll = tol
     x = np.zeros((shape[0], ))
-    solver = krylov.cg
-    #solver = krylov.gmres
+    #solver = krylov.cg
+    solver = krylov.gmres
     #solver = spla.cg
     while la.norm(rhs - Mat(x)) > tol * la.norm(rhs) and ok:
         x = np.zeros((shape[0], ))
@@ -50,9 +50,10 @@ def solve(Mat, rhs, tol=1e-5, Ml=None, force_left=False, check=False):
         x, info = solver(A, b,
                          tol=toll,
                          M=Pl,
-                         residuals=res)
-                         # restrt=None,
-                         # maxiter=shape[0])
+                         residuals=res,
+                         restrt=None,
+                         maxiter=shape[0],
+                         orthog='mgs')
         ts = time() - tt
         print(info, len(res),
               la.norm(rhs - Mat(x)) / la.norm(rhs),
@@ -72,7 +73,7 @@ def solve(Mat, rhs, tol=1e-5, Ml=None, force_left=False, check=False):
     return resn, ts, x
 #
 
-H = [1., 0.75, 0.5, 0.25, 0.1, 0.075] #, 0.05, 0.025]
+H = [5, 10, 20, 30, 40, 50, 75, 100]
 
 Dof = np.zeros((len(H), 7))
 Its = np.zeros((len(H), 7))
@@ -81,7 +82,10 @@ STps = np.zeros((len(H), 7))
 
 for i, h in enumerate(H):
     print(h)
-    grid = bem.shapes.sphere(h=h)
+
+    k = 0.5 * np.pi
+    ll = 2 * np.pi / k
+    grid = bem.shapes.sphere(h=ll/h)
 
     P0 = bem.function_space(grid, "DP", 0)
     P1 = bem.function_space(grid, "P", 1)
@@ -89,13 +93,13 @@ for i, h in enumerate(H):
     P0d = bem.function_space(grid, "DUAL", 0)
     P1b = bem.function_space(grid, "B-P", 1)
 
-    opV00 = bem.operators.boundary.laplace.single_layer(P0, P1, P0)
-    # opW00 = bem.operators.boundary.helmholtz.hypersingular(P0, P1, P0,
-    #                                                        wave_number=0.1)
+    opV00 = bem.operators.boundary.helmholtz.single_layer(P0, P1, P0, k)
+    opW00 = bem.operators.boundary.helmholtz.hypersingular(P0, P1, P0, k,
+                                                           use_slp=True)
     opJ00 = bem.operators.boundary.sparse.identity(P0, P1, P0)
 
-    opV = bem.operators.boundary.laplace.single_layer(P0d, P1b, P0d)
-    opW = bem.operators.boundary.laplace.hypersingular(P1b, P0d, P1b)
+    opV = bem.operators.boundary.helmholtz.single_layer(P0d, P1b, P0d, k)
+    opW = bem.operators.boundary.helmholtz.hypersingular(P1b, P0d, P1b, k)
 
     opJ = bem.operators.boundary.sparse.identity(opV.domain,
                                                  opW.dual_to_range,
@@ -114,19 +118,18 @@ for i, h in enumerate(H):
     tt = time()
     J00w = opJ00.weak_form()
     J00 = J00w.sparse_operator
+    J00 = J00.astype(np.complex)
     iJ00lu = spla.splu(J00)
     iJ00 = spla.LinearOperator(iJ00lu.shape, matvec=iJ00lu.solve)
 
     Dof[i, 1], Tps[i, 1] = V00.shape[0], time() - tt
 
-    # print('Assembling W00...')
-    # tt = time()
-    # W00 = opW00.weak_form()
-    # tt_W00 = time() - tt
+    print('Assembling W00...')
+    tt = time()
+    W00 = opW00.weak_form()
+    tt_W00 = time() - tt
 
-    # W00_real = lambda x: -np.real(W00(x))
-
-    # Dof[i, 6], Tps[i, 6] = W00.shape[0], tt_W00
+    Dof[i, 6], Tps[i, 6] = W00.shape[0], tt_W00
 
     print('Assembling V...')
     tt = time()
@@ -138,6 +141,7 @@ for i, h in enumerate(H):
     tt = time()
     Jw = opJ.weak_form()
     J = Jw.sparse_operator
+    J = J.astype(np.complex)
     iJlu = spla.splu(J)
     Jt = J.transpose()
     iJtlu = spla.splu(Jt)
@@ -175,15 +179,16 @@ for i, h in enumerate(H):
     Its[i, 0], STps[i, 0] = len(res_V0), ts_V0
 
     print('Solving iJV0...')
-    res_iJV0, ts_iJV0, x_iJV0 = solve(V00, f0, Ml=lambda x: iJ00(x))
+    res_iJV0, ts_iJV0, x_iJV0 = solve(V00, f0,
+                                      Ml=lambda x: iJ00(x))
 
     Its[i, 1], STps[i, 1] = len(res_iJV0), ts_iJV0
 
-    # print('Solving iJW0 iJV0...')
-    # res_iJWiJV0, ts_iJWiJV0, x_iJWiJV0 = solve(V00, f0,
-    #                                            Ml=lambda x: iJ00(W00_real(iJ00(x))))
+    print('Solving iJW0 iJV0...')
+    res_iJWiJV0, ts_iJWiJV0, x_iJWiJV0 = solve(V00, f0,
+                                               Ml=lambda x: iJ00(W00(iJ00(x))))
 
-    # Its[i, 6], STps[i, 6] = len(res_iJWiJV0), ts_iJWiJV0
+    Its[i, 6], STps[i, 6] = len(res_iJWiJV0), ts_iJWiJV0
 
 
     print('Solving V...')
@@ -240,8 +245,8 @@ axarr[0].loglog(Dof[:, 0], Its[:, 0], 'ko-', label='V | P0',
            linewidth=lw, markersize=ms)
 axarr[0].loglog(Dof[:, 1], Its[:, 1], 'kd--', label='J^-1 V | P0',
            linewidth=lw, markersize=ms)
-# axarr[0].loglog(Dof[:, 6], Its[:, 6], 'ks:', label='J^-1 W_h J^-1 V | P0',
-#            linewidth=lw, markersize=ms)
+axarr[0].loglog(Dof[:, 6], Its[:, 6], 'ks:', label='J^-1 W J^-1 V | P0',
+           linewidth=lw, markersize=ms)
 axarr[0].loglog(Dof[:, 2], Its[:, 2], 'bo-', label='V | dP0',
            linewidth=lw, markersize=ms)
 axarr[0].loglog(Dof[:, 3], Its[:, 3], 'bd--', label='J^-T V | dP0/bP1',
@@ -258,8 +263,8 @@ axarr[0].set_ylabel('Number of Iterations')
 
 axarr[1].loglog(Dof[:, 0], Tps[:, 0], 'ko-', label='V | P0',
            linewidth=lw, markersize=ms)
-# axarr[1].loglog(Dof[:, 6], Tps[:, 6], 'kd--', label='W_h | P0',
-#            linewidth=lw, markersize=ms)
+axarr[1].loglog(Dof[:, 6], Tps[:, 6], 'ks:', label='W | P0',
+           linewidth=lw, markersize=ms)
 axarr[1].loglog(Dof[:, 2], Tps[:, 2], 'bo-', label='V | dP0',
            linewidth=lw, markersize=ms)
 axarr[1].loglog(Dof[:, 5], Tps[:, 5], 'rd--', label='W | bP1',
@@ -270,6 +275,6 @@ axarr[1].legend(loc=2)
 axarr[1].set_ylabel('Assembling CPU time')
 axarr[1].set_xlabel('DoF')
 
-axarr[0].set_title("Laplace k = {}".format(k))
+axarr[0].set_title("Helmholtz k = {}".format(k))
 
 plt.show()
