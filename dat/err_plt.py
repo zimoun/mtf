66fdef04dfa7a624e2078f558ad475facfc4884e
zimoun <zimoun.toutoune@gmail.com>
2016-07-12 01:05:46 +0200

    fix scale conv. vs ref.

--- dat/err_plt.py
+++ dat/err_plt.py
@@ -15,6 +15,7 @@ eps = mat['eps'][0, 0]
 Ndom = mat['Ndom'][0, 0]
 
 Size = mat['Size']
+H = mat['H']
 Ecald, Etrans = mat['Ecald'], mat['Etrans']
 Ecald_mie, Etrans_mie = mat['Ecald_mie'], mat['Etrans_mie']
 dEL2, dEl2 = mat['dEL2'], mat['dEl2']
@@ -25,6 +26,7 @@ nEnL2, nEnl2 = mat['nEnL2'], mat['nEnl2']
 shaper = lambda ar: ar.reshape((ar.shape[1],))
 
 Size = shaper(Size)
+H = shaper(H)
 Ecald, Etrans = shaper(Ecald), shaper(Etrans)
 Ecald_mie, Etrans_mie = shaper(Ecald_mie), shaper(Etrans_mie)
 dEL2, dEl2 = shaper(dEL2), shaper(dEl2)
@@ -33,6 +35,7 @@ dEnL2, dEnl2 = shaper(dEnL2), shaper(dEnl2)
 nEnL2, nEnl2 = shaper(nEnL2), shaper(nEnl2)
 
 iSize = 1. / Size
+iH = 1. / H
 
 Nlambdas = shaper(mat['Nlambdas'])
 
@@ -89,3 +92,37 @@ sio.savemat('err_eps{0}_k{1}_Ndom{2}.mat'.format(eps, k, Ndom),
              'dEnL2':dEnL2, 'nEnL2':nEnL2,
              'dEnl2':dEnl2, 'nEnl2':nEnl2,
             })
+
+
+myfig = plt.figure()
+lw, ms = 3, 10
+
+ax = myfig.add_subplot(111)
+
+ax.loglog(iH, H**2, 'k--', linewidth=0.5, markersize=ms)
+
+ax.loglog(iH, dEL2, 'r--', linewidth=lw, markersize=ms, label='L2 Dir.')
+
+if dEnl2[0] < 1e5:
+    ax.loglog(iH, dEnl2, 'rd', linewidth=lw, markersize=ms, label='normalized l2 Dir.')
+    ax.loglog(iH, dEnL2, 'r-', linewidth=lw, markersize=ms, label='normalized L2 Dir.')
+
+ax.loglog(iH, nEL2, 'b--', linewidth=lw, markersize=ms, label='L2 Neu.')
+
+if nEnl2[0] < 1e5:
+    ax.loglog(iH, nEnl2, 'bd', linewidth=lw, markersize=ms, label='normalized l2 Neu.')
+    ax.loglog(iH, nEnL2, 'b-', linewidth=lw, markersize=ms, label='normalized L2 Neu.')
+
+ax.loglog(iH, Ecald, 'm-', linewidth=lw, markersize=ms, label='Calderon l2 Sol.')
+ax.loglog(iH, Etrans, 'mv', linewidth=lw, markersize=ms, label='Transmission l2 Sol.')
+
+ax.loglog(iH, Ecald_mie, 'cs-', linewidth=lw, markersize=ms, label='Calderon l2 Mie')
+ax.loglog(iH, Etrans_mie, 'g*-',linewidth=lw, markersize=ms, label='Transmission l2 Mie')
+
+ax.set_xlabel('1/h')
+ax.set_ylabel('Error')
+
+ax.grid(True, which="both")
+ax.legend(bbox_to_anchor=(1.1, 1.05))
+
+myfig.savefig('err_conv.eps')
